270ebed59f514c58d6388da680a8ca424aeb0bd8
joel <joel2000nickson@gmail.com>
2021-08-29 19:16:04 +0530

    fixed err's and integrated pg

--- main.py
+++ main.py
@@ -1,38 +1,36 @@
 from tkinter import *
 from tkinter.font import Font
 
-from functools import partial
-import os
+from postg import pg
 
-import psycopg2
+
+root = Tk()
+username = StringVar()
+password = StringVar() 
 
 def authenticate(username, passw):
-    con = psycopg2.connect(database=os.environ.get('DBNAME'), user=os.environ.get('DBUSER'), password=os.environ.get('DBPASS'),
-                           host=os.environ.get('DBHOST'),port='5432')
-    cursor = con.cursor()
-
-    cursor.execute(f'SELECT pass FROM usertable WHERE name=\'{username}\';')
-
-    result = cursor.fetchone()
+    result = pg(f"SELECT pass FROM usertable WHERE name='{username}';",cond='one') 
 
     print("Username:", username, "Password:", passw)
+    print(result,"\n\n")
 
-    if result == passw:
-        AUTHENTICATED = True
-        print(AUTHENTICATED)
-    
-    AUTHENTICATED = False
-    print(AUTHENTICATED)
+    if result!=None and len(result)>0 and result[0] == passw:
+       print("Authenticated =",True)
+    else:
+        print("Authenticated =",False)
+
+
+def button_load():
+    authenticate(username.get(), password.get())
+    print("btn call complete")
 
 
 def main():
-
-    root = Tk()
     root.title("Password Manager")
     root.geometry("500x600")
     root.configure(background='black')
 
-    lobsterFont = Font(family='Lobster Regular', size=40)
+    lobsterFont = Font(family='Lobster', size=40)
     opensansSemiBold = Font(family='OpenSans-SemiBold', size=10)
 
     titleLabel = Label(root, text="PassMan")
@@ -41,26 +39,22 @@ def main():
 
     usernameLabel = Label(root, text="Username")
     usernameLabel.configure(background='black', font=opensansSemiBold, fg='white')
-    # username = StringVar()
-    usernameEntry = Entry(root)
+    usernameEntry = Entry(root, textvariable=username)
     usernameEntry.configure(background='white', fg='black')
     usernameLabel.place(relx=0.5, rely=0.46, anchor=CENTER)
     usernameEntry.place(relx=0.5, rely=0.5, anchor=CENTER)
 
     passwordLabel = Label(root, text="Password")
     passwordLabel.configure(background='black', font=opensansSemiBold, fg='white')
-    # password = StringVar()
-    passwordEntry = Entry(root)
+    passwordEntry = Entry(root, textvariable=password, show='*')
     passwordEntry.configure(background='white', fg='black')
     passwordLabel.place(relx=0.5, rely=0.6, anchor=CENTER)
     passwordEntry.place(relx=0.5, rely=0.64, anchor=CENTER)
 
-    validateLogin = partial(authenticate, usernameEntry.get(), passwordEntry.get())
-
-    loginButton = Button(root, text='Login', command=validateLogin)
+    loginButton = Button(root, text='Login', command=button_load)
     loginButton.place(relx=0.5, rely=0.7, anchor=CENTER)
 
     root.mainloop()
 
-if __name__ == "__main__":
-    main()
+
+main()
